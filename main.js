1985358c9ce9a1af65dde950de98172fc716caae
wikkiboi <mofouyu@gmail.com>
2024-04-07 22:52:16 -0700

    Added styling to divs and eventListener that changes background color when hovering over grid

--- main.js
+++ main.js
@@ -5,13 +5,20 @@ const container = document.querySelector('.container');
 const createGrid = (amtofGrids) => {
     //create for loop to create rows left to right
     for (let i = 0; i < amtofGrids; i++) {
-        const row = document.createElement("div");
-        row.classList.add("grid-row");
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
 
         //loop for columns in one row; finish loop before returning to outer loop
         for (let j = 0; j < amtofGrids; j++) {
-            const gridBox = document.createElement("div");
-            gridBox.classList.add("grid-box");
+            const widthAndHeight = 960 / sizeofGrid; //changes grid dynamically based of sizeofGrid
+            const gridBox = document.createElement('div');
+            gridBox.classList.add('grid-box');
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
+
+            gridBox.addEventListener('mouseover', () => {
+                gridBox.style.backgroundColor = 'black';
+            })
             row.appendChild(gridBox);
         }
         
